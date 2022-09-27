8a7862a0c3eb04abd9ae02912bd009247c04733e
Artem <artemkostrikin12@gmail.com>
2022-09-27 19:33:00 +0300

    дз

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using static System.Console;
+Clear();
+Write("Введите число: ");
+int Number = Convert.ToInt32(ReadLine());
+int count = 1;
+while(count <= Number)
+{
+Write($"{Math.Pow(count,3)} ");
+count++;
+}
